c35fc6840da935dd66c97c0b7b33e6488b4675c9
Dale Emery <dale@dhemery.com>
2012-07-16 16:50:02 -0700

    Document exception classes.

--- src/main/java/com/dhemery/victor/HttpException.java
+++ src/main/java/com/dhemery/victor/HttpException.java
@@ -1,5 +1,8 @@
 package com.dhemery.victor;
 
+/**
+ * Reports that Victor had a problem while communicating over HTTP.
+ */
 public class HttpException extends RuntimeException {
     public HttpException(String explanation, Throwable cause) {
         super(explanation, cause);
--- src/main/java/com/dhemery/victor/OSCommandException.java
+++ src/main/java/com/dhemery/victor/OSCommandException.java
@@ -1,6 +1,8 @@
 package com.dhemery.victor;
 
-
+/**
+ * Reports that Victor had a problem while running an OS command.
+ */
 public class OSCommandException extends RuntimeException {
     public OSCommandException(OSCommand command, Throwable cause) {
         super(String.format("Exception while executing shell command %s", command.description()), cause);
