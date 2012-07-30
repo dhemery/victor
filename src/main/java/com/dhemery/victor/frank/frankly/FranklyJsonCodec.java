07ab7de7570a350f8ff0517d536500d88994125f
Dale Emery <dale@dhemery.com>
2012-07-30 14:29:58 -0700

    Maven. Forgive me.

--- src/main/java/com/dhemery/victor/frank/frankly/FranklyJsonCodec.java
+++ src/main/java/com/dhemery/victor/frank/frankly/FranklyJsonCodec.java
@@ -1,6 +1,6 @@
 package com.dhemery.victor.frank.frankly;
 
-import com.dhemery.network.Codec;
+import com.dhemery.serializing.Codec;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
