ed7c11648650887eeaed7df6d42d0862b18d60aa
Dale Emery <dale@dhemery.com>
2012-08-26 17:21:05 -0700

    Finish FranklyFrank.map() tests

--- src/test/java/com/victor/frank/tests/FranklyFrankTest.java
+++ src/test/java/com/victor/frank/tests/FranklyFrankTest.java
@@ -15,8 +15,10 @@ import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 
@@ -57,7 +59,7 @@ public class FranklyFrankTest {
     public void accessibilityCheckReturnsTrueIfEndpointResponseIsTrue() {
         context.checking(new Expectations(){{
             oneOf(endpoint).get("/accessibility_check", AccessibilityCheckResponse.class);
-            will(returnValue(new AccessibilityCheckResponse(true)));
+                will(returnValue(new AccessibilityCheckResponse(true)));
         }});
 
         assertThat(frank.accessibilityCheck(), is(true));
@@ -68,7 +70,7 @@ public class FranklyFrankTest {
     public void appExecCallsEndpointPutAppExec() {
         context.checking(new Expectations(){{
             oneOf(endpoint).put(with("/app_exec"), with(any(AppExecOperation.class)), with(any(Class.class)));
-            will(returnValue(anAppExecResponse()));
+                will(returnValue(anAppExecResponse()));
         }});
 
         frank.appExec(IGNORED_METHOD_NAME);
@@ -79,7 +81,7 @@ public class FranklyFrankTest {
         final String methodName = "theMethodName";
         context.checking(new Expectations(){{
             oneOf(endpoint).put(with("/app_exec"), with(appExecOperation(methodName)), with(any(Class.class)));
-            will(returnValue(anAppExecResponse()));
+                will(returnValue(anAppExecResponse()));
         }});
 
         frank.appExec(methodName);
@@ -95,7 +97,7 @@ public class FranklyFrankTest {
 
         context.checking(new Expectations(){{
             oneOf(endpoint).put(with("/app_exec"), with(expectedOperation), with(any(Class.class)));
-            will(returnValue(anAppExecResponse()));
+                will(returnValue(anAppExecResponse()));
         }});
 
         frank.appExec(methodName, parameter1, parameter2, parameter3);
@@ -106,7 +108,7 @@ public class FranklyFrankTest {
         final String methodName = "ignoredMethodName";
         context.checking(new Expectations(){{
             allowing(endpoint).put(with("/app_exec"), with(any(AppExecOperation.class)), with(MessageResponse.class));
-            will(returnValue(anAppExecResponse()));
+                will(returnValue(anAppExecResponse()));
         }});
 
         frank.appExec(methodName);
@@ -118,11 +120,22 @@ public class FranklyFrankTest {
         final String theFirstReturnedResult = "the first result";
         context.checking(new Expectations(){{
             allowing(endpoint).put("/app_exec", appExecOperation(methodName), MessageResponse.class);
-            will(returnValue(aMessageResponseWith(theFirstReturnedResult)));
+                will(returnValue(aMessageResponseWith(theFirstReturnedResult)));
         }});
 
         String frankResult = frank.appExec(methodName);
         assertThat(frankResult, is(equalTo(theFirstReturnedResult)));
+    }
+
+    @Test(expected = IosOperationException.class)
+    public void appExecThrowsIfRequestFails() {
+        final String methodName = "ignoredMethodName";
+        context.checking(new Expectations() {{
+            allowing(endpoint).put("/app_exec", appExecOperation(methodName), MessageResponse.class);
+                will(returnValue(aMessageFailureResponse()));
+        }});
+
+        frank.appExec(methodName);
     }
 
     @Test
@@ -138,8 +151,8 @@ public class FranklyFrankTest {
     @Test
     public void mapRequestIncludesEngineName() {
         final String engineName= "the engine name";
+        final MapOperation mapOperation = mapOperation().engine(engineName).build();
         context.checking(new Expectations(){{
-            MapOperation mapOperation = mapOperation().engine(engineName).build();
             oneOf(endpoint).put(with("/map"), with(mapOperation), with(any(Class.class)));
                 will(returnValue(anAppExecResponse()));
         }});
@@ -147,21 +160,44 @@ public class FranklyFrankTest {
         frank.map(engineName, IGNORED_QUERY, IGNORED_METHOD_NAME);
     }
 
-//    @Test
-//    public void appExecRequestIncludesParameters() {
-//        String methodName = "theMethodName";
-//        String parameter1 = "parameter 1";
-//        String parameter2 = "parameter 2";
-//        String parameter3 = "parameter 3";
-//        final AppExecOperation expectedOperation = appExecOperation(methodName, parameter1, parameter2, parameter3);
-//
-//        context.checking(new Expectations(){{
-//            oneOf(endpoint).put(with("/app_exec"), with(expectedOperation), with(any(Class.class)));
-//                will(returnValue(anAppExecResponse()));
-//        }});
-//
-//        frank.appExec(methodName, parameter1, parameter2, parameter3);
-//    }
+    @Test
+    public void mapRequestIncludesQuery() {
+        final String query = "the query name";
+        final MapOperation mapOperation = mapOperation().query(query).build();
+        context.checking(new Expectations(){{
+            oneOf(endpoint).put(with("/map"), with(mapOperation), with(any(Class.class)));
+                will(returnValue(anAppExecResponse()));
+        }});
+
+        frank.map(IGNORED_ENGINE, query, IGNORED_METHOD_NAME);
+    }
+
+    @Test
+    public void mapRequestIncludesMethodName() {
+        final String methodName = "the method name";
+        final MapOperation mapOperation = mapOperation().method(methodName).build();
+        context.checking(new Expectations(){{
+            oneOf(endpoint).put(with("/map"), with(mapOperation), with(any(Class.class)));
+                will(returnValue(anAppExecResponse()));
+        }});
+
+        frank.map(IGNORED_ENGINE, IGNORED_QUERY, methodName);
+    }
+
+    @Test
+    public void mapRequestIncludesParameters() {
+        String parameter1 = "parameter 1";
+        String parameter2 = "parameter 2";
+        String parameter3 = "parameter 3";
+        final MapOperation mapOperation = mapOperation().arguments(parameter1, parameter2, parameter3).build();
+
+        context.checking(new Expectations(){{
+            oneOf(endpoint).put(with("/map"), with(mapOperation), with(any(Class.class)));
+                will(returnValue(anAppExecResponse()));
+        }});
+
+        frank.map(IGNORED_ENGINE, IGNORED_QUERY, IGNORED_METHOD_NAME, parameter1, parameter2, parameter3);
+    }
 
     @Test
     public void mapEndpointRequestSpecifiesMessageResponseReturnType() {
@@ -173,28 +209,28 @@ public class FranklyFrankTest {
         frank.map(IGNORED_ENGINE, IGNORED_QUERY, IGNORED_METHOD_NAME);
     }
 
-//    @Test
-//    public void appExecReturnsFirstResultIfRequestSucceeds() {
-//        final String methodName = "ignoredMethodName";
-//        final String theFirstReturnedResult = "the first result";
-//        context.checking(new Expectations(){{
-//            allowing(endpoint).put("/app_exec", appExecOperation(methodName), MessageResponse.class);
-//                will(returnValue(aMessageResponseWith(theFirstReturnedResult)));
-//        }});
-//
-//        String frankResult = frank.appExec(methodName);
-//        assertThat(frankResult, is(equalTo(theFirstReturnedResult)));
-//    }
-//
-    @Test(expected = IosOperationException.class)
-    public void appExecThrowsIfRequestFails() {
-        final String methodName = "ignoredMethodName";
-        context.checking(new Expectations() {{
-            allowing(endpoint).put("/app_exec", appExecOperation(methodName), MessageResponse.class);
-            will(returnValue(aMessageFailureResponse()));
+    @Test
+    public void mapReturnsResultsIfRequestSucceeds() {
+        final String result1 = "result 1";
+        final String result2 = "result 2";
+        final String result3 = "result 3";
+        context.checking(new Expectations(){{
+            allowing(endpoint).put("/map", mapOperation().build(), MessageResponse.class);
+                will(returnValue(aMessageResponseWith(result1, result2, result3)));
         }});
 
-        frank.appExec(methodName);
+        List<String> frankResult = frank.map(IGNORED_ENGINE, IGNORED_QUERY, IGNORED_METHOD_NAME);
+        assertThat(frankResult, containsInAnyOrder(result1, result2, result3));
+    }
+
+    @Test(expected = IosOperationException.class)
+    public void mapThrowsIfRequestFails() {
+        context.checking(new Expectations() {{
+            allowing(endpoint).put(with("/map"), with(any(MapOperation.class)), with(any(Class.class)));
+                will(returnValue(aMessageFailureResponse()));
+        }});
+
+        frank.map(IGNORED_ENGINE, IGNORED_QUERY, IGNORED_METHOD_NAME);
     }
 
     @Test
@@ -230,7 +266,7 @@ public class FranklyFrankTest {
         return new MessageResponse(true, Collections.<String>emptyList(), null, null);
     }
 
-    private MessageResponse aMessageResponseWith(String result) {
+    private MessageResponse aMessageResponseWith(String... result) {
         return new MessageResponse(true, Arrays.asList(result), null, null);
     }
 
